802f259c0fb4ecc0afe82116a72802dbb15f7e21
Dilan Morar <DMorar@spartaglobal.com>
2019-12-09 21:00:28 +0000

    created code to output 10 objects

--- NewUsers_class.py
+++ NewUsers_class.py
@@ -1,11 +1,5 @@
 class NewUsers():
-    def __init__(self,file_name):
+    def __init__(self, file_name):
         self.file_name = file_name
-        try:
-            with open(file_name, 'r')
-                file_to_write.write(item + '\n')
-        except FileNotFoundError as error:
-            print('Check your file/path')
-            print(error)
-        finally:
-            print('Program ending')
+
+
--- run_text.py
+++ run_text.py
@@ -1,10 +1,25 @@
 from NewUsers_class import *
 
 try:
-    with open(file_name, 'r')
-        file_to_write.write(item + '\n')
+    names = NewUsers('names.txt')
+    with open(names.file_name, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            print(line.strip('\n'))
+    names.file_name
 except FileNotFoundError as error:
-    print('Check your file/path')
+    print('Check your file')
     print(error)
 finally:
     print('Program ending')
+
+try:
+    names = NewUsers('names.txt')
+    with open(names.file_name, 'a') as file_to_write:
+        file_to_write.write(item + '\n')
+except FileNotFoundError as error:
+    print('Check your file')
+    print(error)
+finally:
+    print('Program ending')
+
